500629b115a634c77c10b8533522d65097e76277
Christian Ingles <christianarguetaaa@gmail.com>
2022-12-23 15:30:01 -0600

    add references to my elements

---
+++ js/app.js
@@ -0,0 +1,5 @@
+const counterShow = document.querySelector("#counterShow");
+const increase = document.querySelector("#increase");
+const reset = document.querySelector("#reset");
+const decrease = document.querySelector("#decrease");
+let initialCounter = 0;
